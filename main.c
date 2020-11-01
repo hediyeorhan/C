6e0bee3611196047666ea1b3171c5317159f231b
Hediye Orhan <hediyeorhan2015@gmail.com>
2020-11-01 10:31:30 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Bankamatik:
+–Kendisine parametre olarak gelen YTL cinsinden para miktarını 20, 10 ve 5 TL'lik birimler halinde hesaplar.
+–Girilen miktar 5 YTL'nin bir katı değilse, ekrana uygun bir mesaj gönderir.
+–Bankamatik fonksiyonunu yazınız.*/
+int banka(int para)
+{
+
+  if(para%5==0)
+    {
+    int beslik,onluk,yirmilik;
+    yirmilik=para/20;
+    para=para-(yirmilik*20);
+    onluk=para/10;
+    para=para-(onluk*10);
+    beslik=para/5;
+    para=para-(beslik*5);
+    printf("Yirmilik = %d tane\n",yirmilik);
+    printf("Onluk = %d tane\n",onluk);
+    printf("Beslik = %d tane\n",beslik);
+    }
+      else
+      {
+      printf("Lutfen 5'in kati olan bir sayi giriniz.!");
+      }
+     return para;
+}
+
+int main()
+{
+    int x;
+    printf("Cekilecek para miktarini giriniz:");
+    scanf("%d",&x);
+    printf("%d",banka(x));
+    return 0;
+}
