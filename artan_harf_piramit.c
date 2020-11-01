94afd795b6a465819a8c9e85f5cf03fa39a9b9be
Hediye Orhan <hediyeorhan2015@gmail.com>
2020-11-01 10:31:11 +0300

    Update and rename main.c to artan_harf_piramit.c

--- artan_harf_piramit.c
+++ artan_harf_piramit.c
@@ -17,22 +17,6 @@ int main()
        ++alfabe;
        printf("\n");
    }
-   /*
-   int i, j;
-    char input, alphabet = 'A';
 
-    printf("Son satirda yazdirmak istediginiz buyuk karakteri girin: ");
-    scanf("%c",&input);
-
-    for(i=1; i <= (input-'A'+1); ++i)
-    {
-        for(j=1; j<=i; ++j)
-        {
-            printf("%c", alphabet);
-        }
-        ++alphabet;
-
-        printf("\n");
-    }*/
     return 0;
 }
